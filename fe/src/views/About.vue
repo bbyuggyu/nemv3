d74016548843a49cdc7a58fcd391db0fcd5dbc8c
bbyuggyu <bbyuggyu@gmail.com>
2019-11-05 19:37:52 +0900

    start project

--- fe/src/views/About.vue
+++ fe/src/views/About.vue
@@ -1,5 +1,65 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container fluid>
+    <v-row dense v-for="u in users"  v-bind:key="u">
+      <v-card
+        class="mx-auto"
+        max-width="400"
+      >
+        <v-img
+          class="white--text align-end"
+          height="200px"
+          src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+        >
+          <v-card-title>{{u.name}}</v-card-title>
+        </v-img>
+
+        <v-card-subtitle class="pb-0">{{u.age}}</v-card-subtitle>
+
+        <v-card-text class="text--primary">
+          <div>Whitehaven Beach</div>
+
+          <div>Whitsunday Island, Whitsunday Islands</div>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-btn
+            color="orange"
+            text
+          >
+            Share
+          </v-btn>
+
+          <v-btn
+            color="orange"
+            text
+          >
+            Explore
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-row>
+  </v-container>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      users: []
+    }
+  },
+
+  mounted () {
+    axios.get('http://localhost:3000/api/user')
+      .then((r) => {
+        this.users = r.data.users
+        console.log(r)
+      })
+      .catch((e) => {
+        console.error(e.message)
+      })
+  }
+}
+</script>
