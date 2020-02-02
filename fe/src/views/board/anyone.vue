33ac185645201724e93138162dc191eace189178
bbyuggyu <bbyuggyu@gmail.com>
2020-02-02 19:19:40 +0900

    69 error

--- fe/src/views/board/anyone.vue
+++ fe/src/views/board/anyone.vue
@@ -21,17 +21,114 @@
           </v-img>
         </v-card>
       </v-flex>
+      <!-- <v-flex xs12 sm6 md4 v-for="article in articles" :key="article._id">
+        {{article}}
+      </!--> -->
+     <v-flex xs12>
+       <v-data-table
+          :headers="headers"
+          :items="articles"
+          :loading="loading">
+          <template slot="items" slot-scope="props">
+            <td :class="headers[0].class">{{ id2date(props.item._id)}}</td>
+            <td :class="headers[1].class">{{ props.item.title }}</td>
+            <td :class="headers[2].class">{{ props.item._user ? props.item._user.id : '손님' }}</td>
+            <td :class="headers[3].class">{{ props.item.cnt.view }}</td>
+            <td :class="headers[4].class">{{ props.item.cnt.like }}</td>
+          </template>
+        </v-data-table>
+     </v-flex>
     </v-layout>
+
+    <v-btn
+      color="pink"
+      dark
+      small
+      absolute
+      bottom
+      right
+      fab
+      @click="addDialog"
+    >
+      <v-icon>+</v-icon>
+    </v-btn>
+    <v-dialog v-model="dialog" persistent max-width="500px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">글 작성</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container grid-list-md>
+            <v-layout wrap>
+              <v-flex xs12>
+                <v-text-field
+                  label="제목"
+                  persistent-hint
+                  required
+                  v-model="form.title"
+                ></v-text-field>
+              </v-flex>
+              <v-flex xs12>
+                <v-textarea
+                  label="내용"
+                  persistent-hint
+                  required
+                  v-model="form.content"
+                ></v-textarea>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="green darken-1" text @click="add()">확인</v-btn>
+          <v-btn color="red darken-1" text @click.native="dialog = false">취소</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-snackbar
+      v-model="sb.act"
+    >
+      {{ sb.msg }}
+      <v-btn
+        :color="sb.color"
+        text
+        @click="sb.act = false"
+      >
+        닫기
+      </v-btn>
+    </v-snackbar>
   </v-container>
 </template>
 <script>
 
 export default {
+  components: { },
   data () {
     return {
       board: {
         name: '로딩중...',
         rmk: '무엇?'
+      },
+      headers: [
+        { text: '날짜', value: '_id', sortable: true, class: 'hidden-sm-and-down' },
+        { text: '제목', value: 'title', sortable: true },
+        { text: '글쓴이', value: '_user', sortable: false },
+        { text: '조회수', value: 'cnt.view', sortable: true },
+        { text: '추천', value: 'cnt.like', sortable: true }
+      ],
+      articles: [],
+      loading: false,
+      dialog: false,
+      lvs: [0, 1, 2, 3],
+      form: {
+        title: '',
+        content: ''
+      },
+      sb: {
+        act: false,
+        msg: '',
+        color: 'error'
       }
     }
   },
@@ -39,15 +136,52 @@ export default {
     this.get()
   },
   methods: {
+    addDialog () {
+      this.dialog = true
+      this.form = {
+        title: '',
+        content: ''
+      }
+    },
     get () {
       this.$axios.get('board/아무나')
         .then(({ data }) => {
           if (!data.success) throw new Error(data.msg)
           this.board = data.d
+          this.list()
         })
         .catch((e) => {
           this.pop(e.message, 'error')
         })
+    },
+    add () {
+      if (!this.form.title) return this.pop('제목을 작성해주세요', 'warning')
+      if (!this.form.content) return this.pop('내용을 작성해주세요', 'warning')
+      this.$axios.post(`article/${this.board._id}`, this.form)
+        .then((r) => {
+          this.dialog = false
+          this.list()
+        })
+        .catch((e) => {
+          this.pop(e.message, 'error')
+        })
+    },
+    list () {
+      if (this.loading) return
+      this.loading = true
+      this.$axios.get(`article/${this.board._id}`)
+        .then(({ data }) => {
+          this.articles = data.ds
+          this.loading = false
+        })
+        .catch((e) => {
+          this.pop(e.message, 'error')
+          this.loading = false
+        })
+    },
+    id2date (val) {
+      if (!val) return '잘못된 시간 정보'
+      return new Date(parseInt(val.substring(0, 8), 16) * 1000).toLocaleString()
     },
     pop (m, c) {
       this.sb.act = true
